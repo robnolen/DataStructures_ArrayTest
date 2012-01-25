881522db0522efb76fdfe62865ffb4a7f2b238c7
Rob Nolen <rob.nolen@gmail.com>
2012-01-24 19:53:42 -0800

    Initial Commit

---
+++ arraytest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  arraytest
+//
+//  Created by Robert Nolen on 1/24/12.
+//  Copyright 2012 Greencrayon.org. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
